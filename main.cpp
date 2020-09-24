36542d459b00a76f1eb7867843f86d59b3bf6bca
rtp99 <ryan.perkins3@cwu.edu>
2020-09-24 15:00:14 -0700

    Added Unversioned Files

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include "main.h"
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
+
+int takeDataSet(std::string file_name) {
+
+    return 0;
+}
---
+++ main.h
@@ -0,0 +1,7 @@
+#ifndef CS456LAB2_MAIN_H
+#define CS456LAB2_MAIN_H
+
+#include <iostream>
+
+
+#endif //CS456LAB2_MAIN_H
