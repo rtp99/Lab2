9348702076668450a7a4e9936930280b9e5593b0
rtp99 <ryan.perkins3@cwu.edu>
2020-09-24 15:13:11 -0700

    added function signatures

--- main.h
+++ main.h
@@ -4,6 +4,10 @@
 #include <iostream>
 #include <fstream>
 
+int checkFileValidity(std::string file_location); // checks if file is in appropriate format, returns number of attributes
+double** createDataset(std::string file_location, int num_attr); // creates the dataset from given information
 
+void regression (double ** data_set, int num_attr); // primary output is over text console, no real return
+double RMSE(double * predicted, double * actual, int num_vals); // determines general RMSE from a set of given and predicted values of length num_vals
 
 #endif //CS456LAB2_MAIN_H
