8b51d468847d9d54b00a2ba2a94282c123f5d1a8
rtp99 <ryan.perkins3@cwu.edu>
2020-09-24 15:20:13 -0700

    fixed file read

--- main.cpp
+++ main.cpp
@@ -1,20 +1,23 @@
 #include "main.h"
 
 int main(int argc, char** argv) {
-    std::ifstream target_file;
-    // check argument count
     if(argc != 2) {
         std::cout << "Error: Invalid Argument Count" << std::endl;
         return 1;
     }
+    int num_attr = checkFileValidity(argv[1]);
+    return 0;
+}
+
+int checkFileValidity(std::string file_location) {
+    std::ifstream target_file;
     // Check if file is a csv
-    std::string to_process = argv[1];
-    if(to_process.substr(to_process.length() - 4, to_process.length()) != ".csv"){
+    if(file_location.substr(file_location.length() - 4, file_location.length()) != ".csv"){
         std::cout << "Error: File is not a .csv" << std::endl;
         return 1;
     }
     // open file and test
-    target_file.open(argv[1]);
+    target_file.open(file_location);
     if (target_file.is_open()) {
         std::cout << "File accepted, reading now" << std::endl ;
     } else {
